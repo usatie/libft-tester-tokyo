e40c4f17ef4796966b8aeed65860c6d55dfb3a2e
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-04-15 17:10:31 +0900

    :sparkles: Add strlen very long cases

--- srcs/test_ft_strlen.c
+++ srcs/test_ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 16:58:08 by susami            #+#    #+#             */
-/*   Updated: 2022/04/15 17:05:15 by susami           ###   ########.fr       */
+/*   Updated: 2022/04/15 17:10:29 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,12 @@ int	main(void)
 	char *s;
 	s = calloc((size_t)INT_MAX + 10, sizeof(char));
 	memset(s, 1, (size_t)INT_MAX - 5);
-	/* 4 */ ASSERT_EQ_I(ft_strlen(s), strlen(s));
+	/* 4 very long */ ASSERT_EQ_I(ft_strlen(s), strlen(s));
 	memset(s, 1, (size_t)INT_MAX + 5);
-	/* 5 */ ASSERT_EQ_I(ft_strlen(s), strlen(s));
+	/* 5 int overflow */ ASSERT_EQ_I(ft_strlen(s), strlen(s));
+	for (int i = 1; i < INT_MAX; i++)
+		s[i - 1] = i;
+	/* 6 various values */ ASSERT_EQ_I(ft_strlen(s), strlen(s));
 	// TODO: assert segmentation fault
-	// /* 6 */ ASSERT_EQ_I(ft_strlen(NULL), strlen(NULL));
+	// /* 7 */ ASSERT_EQ_I(ft_strlen(NULL), strlen(NULL));
 }
