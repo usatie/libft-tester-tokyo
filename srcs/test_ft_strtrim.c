ed437ab17d606021f20f76287f9f300c7381ff10
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-04-18 16:33:34 +0900

    :sparkles: Add strtrim tests

---
+++ srcs/test_ft_strtrim.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_strtrim.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/18 16:26:25 by susami            #+#    #+#             */
+/*   Updated: 2022/04/18 16:32:14 by susami           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+#include "libassert.h"
+#include <malloc/malloc.h>
+
+int	main(void)
+{
+	/* 1 */ ASSERT_EQ_STR(ft_strtrim("hello world", "world"), "hello ");
+	/* 2 */ ASSERT_EQ_STR(ft_strtrim("hello world", "hello"), " world");
+	/* 3 */ ASSERT_EQ_STR(ft_strtrim("hello world", ""), "hello world");
+	/* 4 */ ASSERT_EQ_STR(ft_strtrim("", ""), "");
+	/* 5 */ ASSERT_EQ_STR(ft_strtrim("    hello world     ", " "), "hello world");
+	/* 6 */ ASSERT_EQ_STR(ft_strtrim(" \n\t\r    hello \n\t\r world     \r\t\n\t \r\n", " \n\t\r"), "hello \n\t\r world");
+	/* 7 */ ASSERT_EQ_STR(ft_strtrim("hello world", "abcdefghijklmnopqrstuvwxyz"), " ");
+}
