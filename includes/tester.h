ef2a04446493b83a945c649ca3466f253d7b7f99
Shun Usami <susami@c1r19s8.42tokyo.jp>
2022-05-30 12:29:00 +0900

    :bug: Fix header "#include <limits.h>"

--- includes/tester.h
+++ includes/tester.h
@@ -6,13 +6,14 @@
 /*   By: susami <susami@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 09:47:36 by susami            #+#    #+#             */
-/*   Updated: 2022/05/02 10:07:55 by susami           ###   ########.fr       */
+/*   Updated: 2022/05/30 12:25:38 by susami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TESTER_H
 # define TESTER_H
 
+# include <limits.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
